8c8a132d3c6038e38c407fe4b5652bcd5e7047bb
Jonathan Zungre <jzungre@gmail.com>
2016-12-11 22:12:59 -0500

    parses json object, randomizes qquestions

--- Client/src/questionController.js
+++ Client/src/questionController.js
@@ -1,5 +1,30 @@
 angular.module('superActorQuiz.question', [])
-// controller for bounty submission form page, aka form html
-.controller('questionController', function ($scope, $http, $window) {
 
+.controller('questionController', function ($scope, $http, $window) {
+	console.log('in questionController')
+	$scope.movie1 = 'Legends of the Fall';
+	$scope.movie3 = 'Interview with the Vampire';
+	$scope.movie4 = 'Mr. and Mrs. Smith'
+
+	$http.get("themoviedb_data.json")
+    .then(function (response) {
+    	
+    	console.log('results',response.data, 'movie', response.data.results[0].original_title);
+
+    	function getRandomInt(min, max) {
+  			min = Math.ceil(min);
+  			max = Math.floor(max);
+  			return Math.floor(Math.random() * (max - min)) + min;
+		}
+
+
+    	$scope.movie1 = response.data.results[getRandomInt(1,6)].original_title;
+    	$scope.movie3 = response.data.results[3].original_title;
+    	$scope.movie4 = response.data.results[4].original_title;
+   //   	$scope.games = response.data.results[0].title.map(function(item) {
+  	// 		console.log(item)
+  	// 		return item;
+		 // });
+
+	})
 })
